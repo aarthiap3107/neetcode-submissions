e2168168d288585543f9cde5e4e1d7326df54fb5
aarthiap3107 <aarthzzz3107@gmail.com>
2026-06-09 00:35:00 +0530

    Add: maximum-subarray - submission-5

---
+++ Data Structures & Algorithms/maximum-subarray/submission-5.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxsum=nums[0];
+        int cursum=0;
+        for(int i=0;i<nums.length;i++){
+            if(cursum<0){
+                cursum=0;
+            }
+            cursum+=nums[i];
+            if(cursum>maxsum){
+                maxsum=cursum;
+            }
+        }
+        return maxsum;
+    }
+}
